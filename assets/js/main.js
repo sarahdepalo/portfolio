f1be920e9214e85afd9abbadc4fcd33ea52d612c
Sarah dePalo <sarahdepalo99@gmail.com>
2021-07-17 15:31:46 -0400

    finished mobile responsiveness and basic portfolio is done

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,34 @@
 //Adds spin class to bee icon
 const beeIcon = document.getElementById('beeIcon');
 beeIcon.addEventListener('click', () => {
-	beeIcon.classList.toggle('spin');
-
+	beeIcon.classList.add('spin');
+	setTimeout(() => {
+		beeIcon.classList.remove('spin');
+	}, 2000);
 });
+
+// Adds Button animations on sroll
+$(window).scroll(function () {
+	if($(window).scrollTop() > 0) {
+	  $(".animated-btn").addClass("animated");
+	  $(".animated-btn").addClass("fadeIn");
+	}
+	else {
+		$(".animated-btn").removeClass("animated");
+		$(".animated-btn").removeClass("fadeIn");
+	}
+  });
+
+  //Adds fade in from left animation to skill icons
+  $(window).scroll(function () {
+	if($(window).scrollTop() > 0) {
+	  $("#skill-icons").addClass("animate__animated");
+	  $("#skill-icons").addClass("animate__fadeInLeftBig");
+	}
+	else {
+		$("#skill-icons").removeClass("animate__animated");
+		$("#skill-icons").removeClass("animate__fadeInLeftBig");
+	}
+  });
+
+
